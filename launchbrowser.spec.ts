612a0c211468a2749b352f4618db7cf1a9c96617
Prasannasrinivas Ganamurthy <prasannasrinivasg@gmail.com>
2026-03-14 19:43:34 +0530

    Add files via upload

---
+++ launchbrowser.spec.ts
@@ -0,0 +1,8 @@
+import {test, chromium } from '@playwright/test';
+test("launch a browser",async ()=>{
+const browser = await chromium.launch();
+const context = await browser.newContext();
+const page = await context.newPage();  
+await page.goto('https://www.amazon.in/');
+await page.waitForTimeout(3000); 
+})
